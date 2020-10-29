e211c8c9582815ffff85df0cf110e6b5cd86c45b
Noah <noahnolte777@gmail.com>
2020-10-28 23:54:06 -0500

    Added blanken_file which takes an input file and puts blanks in

--- BlankMaker.py
+++ BlankMaker.py
@@ -11,3 +11,25 @@ Different input types:
     - Words per line
     - part of speech
 """
+import random
+
+def blanken_file(split_total, file_name = "text-input.txt"):
+    file_text = open(file_name).read() # Opens and reads the file
+    split_text = file_text.split() # Splits the text
+    
+    for i in range(split_total+1):
+        found_word = False
+        while not found_word:
+            pick_from_list = random.randint(0, split_total)
+            if pick_from_list == "___":
+                continue
+            else:
+                split_text[pick_from_list] = "___"
+                found_word = True
+            
+    with open(file_name, "w") as file:
+        file.write(" ".join(split_text))
+    
+    print(split_text)
+            
+blanken_file(5)
