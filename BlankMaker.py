6b9a8935ad9e9de2a5a24d3387b6cd74a25a89f7
Noah <noahnolte777@gmail.com>
2020-10-28 22:59:21 -0500

    Starting docstring

---
+++ BlankMaker.py
@@ -0,0 +1,13 @@
+"""
+THE PLAN:
+
+    1. Input a text file and the amount of words to be blanked
+    2. Output a text file with the amount of words blanked
+
+Different input types:
+    Start with:
+    - Words total
+    Later:
+    - Words per line
+    - part of speech
+"""
