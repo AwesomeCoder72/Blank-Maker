b4aa8e38b90a9a6b378155f67799da9a62a54f7d
Noah <noahnolte777@gmail.com>
2020-11-08 19:15:05 -0600

    Fixed blanken by line

--- BlankMaker.py
+++ BlankMaker.py
@@ -17,37 +17,43 @@ Different input types:
 import random
 import re
 
-def blanken_file_by_line(split_total, file_name = "text-input.txt"):
+file_path = r"\Users\Noah\Documents\Programs\Python Programs\BlankMaker\text-input.txt"
+
+def blanken_file_by_line(split_total, file_name = file_path):
     output_list = []
     with open(file_name, 'r') as file:
         file_lines = file.read().split("\n")
         
-        
-    output_list = [re.findall(r"[\w']+|[.,!?;]", unsplit)
+    output_list = [re.findall(r"[\w']+|[.,!?;:]", unsplit)
                        for unsplit in file_lines]
     print(output_list)
     
     for line in output_list:
-        for i in range(split_total):
+        if len(line) <= split_total:
+            amount_to_split = int(len(line) / 2)
+        else:
+            amount_to_split = int(split_total)
+        for i in range(amount_to_split):
             found_word = False
             while not found_word:
                 pick_from_list = random.randint(0, len(line)-1)
                 if line[pick_from_list] == "___" or\
-            re.match("^[.,!?;]+$", line[pick_from_list]):
+                not line[pick_from_list] or\
+            re.match("^[.,!?;:]+$", line[pick_from_list]):
                     continue
                 else:
                     line[pick_from_list] = "___"
                     found_word = True
     output = "\n".join([re.sub(r' (?=\W)', '', " ".join(i)) for i in 
                         [line for line in output_list]])
-    
+    print(output)
     with open(file_name, "w") as file:
         file.write(output)
             
-def blanken_file_by_total(split_total, file_name = "text-input.txt"):
+def blanken_file_by_total(split_total, file_name = file_path):
     with open(file_name, 'r') as file:
         file_lines = file.read().split("\n")
-    output_list = [re.findall(r"[\w']+|[.,!?;]", unsplit)
+    output_list = [re.findall(r"[\w']+|[.,!?;:]", unsplit)
                        for unsplit in file_lines]
             
     for i in range(split_total):
@@ -56,14 +62,15 @@ def blanken_file_by_total(split_total, file_name = "text-input.txt"):
             pick_from_list = random.randint(0, len(output_list)-1)
             pick_from_line = random.randint(0, len(output_list[pick_from_list])-1)
             if output_list[pick_from_list][pick_from_line] == "___" or\
-            re.match("^[.,!?;]+$", output_list[pick_from_list][pick_from_line]):
+            not output_list[pick_from_list][pick_from_line] or\
+            re.match("^[.,!?;:]+$", output_list[pick_from_list][pick_from_line]):
                 continue
             else:
                 output_list[pick_from_list][pick_from_line] = "___"
                 found_word = True
-    output = "\n".join([re.sub(r' (?=\W)', '', " ".join(i)) for i in output_list])
-
+    output = "\n".join([re.sub(r' (?=\W)', '', " ".join(i)) for i in 
+                        [line for line in output_list]])
     with open(file_name, "w") as file:
         file.write(output)
 
-blanken_file_by_line(2)
+blanken_file_by_line(4)
